86fab75f2b3fc9dd6c314153f598d32f89bc8784
Alexander <maintrineg@gmail.com>
2019-07-27 14:40:54 +0300

    added scope folder

---
+++ app/src/main/java/com/dudencovgmail/splashes/di/scopes/ApplicationScope.kt
@@ -0,0 +1,7 @@
+package com.dudencovgmail.splashes.di.scopes
+
+import javax.inject.Scope
+
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ApplicationScope
---
+++ app/src/main/java/com/dudencovgmail/splashes/di/scopes/FragmentScope.kt
@@ -0,0 +1,8 @@
+package com.dudencovgmail.splashes.di.scopes
+
+import javax.inject.Scope
+import javax.inject.Singleton
+
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class FragmentScope
